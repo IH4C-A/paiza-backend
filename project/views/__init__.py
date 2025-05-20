4086a922ee74cedeced45a8295bda31088434379
NHS22takahashi <nhs20084@nhs.hal.ac.jp>
2025-05-20 12:33:27 +0900

    2025/05/20_12:33

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,4 +1,8 @@
 from .user_views import user_bp
+from .answer_views import answer_bp
 
 def register_blueprints(app):
     app.register_blueprint(user_bp)
+
+def register_blueprints(app):
+    app.register_blueprint(answer_bp)
