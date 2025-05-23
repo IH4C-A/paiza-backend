df1b4a9787c605d782eda16ef1fee2d4251dd48c
NHS22takahashi <nhs20084@nhs.hal.ac.jp>
2025-05-23 15:56:13 +0900

    2025/05/23_15:56

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,9 +1,13 @@
 from .user_views import user_bp
 from .answer_views import answer_bp
 from .chats_views import chats_bp
+from .problem_views import problem_bp
+from .comment_views import comment_bp
 
 def register_blueprints(app):
     app.register_blueprint(user_bp)
     app.register_blueprint(chats_bp)
     app.register_blueprint(answer_bp)
+    app.register_blueprint(problem_bp)
+    app.register_blueprint(comment_bp)
     
